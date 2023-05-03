9266029134672719af71c1f92f72291507073dd1
syedibraheem <syedibraheem2626@gmail.com>
2023-05-03 00:52:55 -0700

    first

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var menuicon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+
+menuicon.onclick = function{
+    sidebar.classList.toggle("small-sidebar");
+}
