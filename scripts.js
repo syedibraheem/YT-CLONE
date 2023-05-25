b40988ea545c05351eb0418483440b2c47afc98e
syedibraheem <syedibraheem2626@gmail.com>
2023-05-24 22:26:33 -0700

    d

--- scripts.js
+++
@@ -1,9 +0,0 @@
-var menuicon = document.querySelector(".menu-icon");
-var sidebar = document.querySelector(".sidebar");
-var sidebar = document.querySelector(".container");
-
-
-menuicon.onclick = function{
-    sidebar.classList.toggle("small-sidebar");
-    container.classList.toggle("large-container")
-}
